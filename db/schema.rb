4b6fecddd5f1536803b6bdb6c47e57e00cf21bdf
volodya <murzak.vv@gmail.com>
2015-12-09 13:34:54 +0200

    remove email field from users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151207142722) do
+ActiveRecord::Schema.define(version: 20151209113106) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "username"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 20151207142722) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -36,7 +35,6 @@ ActiveRecord::Schema.define(version: 20151207142722) do
     t.string   "username"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   add_index "users", ["username"], name: "index_users_on_username", unique: true
 
